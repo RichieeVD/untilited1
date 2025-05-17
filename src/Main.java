9b508ec75fd0c39a293382859b37b50af2cd231a
rvd <nseries_nokia@list.ru>
2025-05-17 05:45:11 +0400

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 
-        System.out.println("dog" + dog);
-        System.out.println("cat" + cat);
-        System.out.println("paper" + paper);
+        System.out.println("dog " + dog);
+        System.out.println("cat " + cat);
+        System.out.println("paper " + paper);
     }
 }
